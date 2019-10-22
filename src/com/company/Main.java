8205c8565fbf6ed4a81882ecb9f0a7057d3cac2c
yusufr98 <yusuf090798@gmail.com>
2019-10-22 10:14:55 -0400

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        //declare variables here before we use them
+        int x;
+        double y;
+
+        String firstName, lastName;
+        String movie = "Joker";     //question is initialized</p>
+        //assign values to the variables here
+        x = 21;
+        y = 3.5; //you could also use the constant Integer.MAX_VALUE
+        firstName = "Yusuf";
+        lastName = "Reyazuddin";
+        // String question = "unknown"; //question is initialized
+
+
+        //Use the variables here
+        System.out.println("My name is " + firstName + " " + lastName + " and I am " + x + " years old.");
+        System.out.println("The last movie I saw was " + movie);
+        System.out.println("My GPA is " + y);
+
+    }
+}
